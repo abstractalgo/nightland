96d60388b34c1fe9913c21666c8469bbb332a1b3
Dragan Okanovic <dragan.okan@gmail.com>
2025-04-21 03:20:30 +0200

    First commit (backup)

---
+++ index7b17.php
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">The Night Land - Home</title>
+	<subtitle type="text">The Weird Fiction of William Hope Hodgson</subtitle>
+	<link rel="alternate" type="text/html" href="https://nightland.website"/>
+	<id>https://nightland.website/index.php</id>
+	<updated>2025-04-21T00:27:04+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://nightland.website/index.php?format=feed&amp;type=atom"/>
+	<entry>
+		<title>A Mouse in the Walls of the Lesser Redoubt (Part 1)</title>
+		<link rel="alternate" type="text/html" href="https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt/291-a-mouse-in-the-walls-of-the-lesser-redoubt-part-1"/>
+		<published>2022-07-08T21:44:14+00:00</published>
+		<updated>2022-07-08T21:44:14+00:00</updated>
+		<id>https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt/291-a-mouse-in-the-walls-of-the-lesser-redoubt-part-1</id>
+		<author>
+			<name>Nigel Atkinson</name>
+			<email>coady.k@protonmail.com</email>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;h3 class=&quot;story-teaser&quot;&gt;By &lt;a href=&quot;https://nightland.website/index.php/about/authors-and-artists/109-nigel-atkinson&quot; target=&quot;_blank&quot; rel=&quot;noopener noreferrer&quot;&gt;Nigel Atkinson&lt;/a&gt;&lt;/h3&gt;
+&lt;div class=&quot;story-intro&quot;&gt;
+&lt;p&gt;&lt;img class=&quot;story-thumb&quot; src=&quot;https://nightland.website/images/thumbs/fabian-lesser-redoubt-thumb.jpg&quot; alt=&quot;A traveller beholds a tall pyramid in darkness.&quot; /&gt;&lt;/p&gt;
+&lt;p&gt;In the Lesser Redoubt, when the Master Monstruwacan dies, his designated successor undergoes an ordeal before he can succeed to his office. But who does he converse with? Are the visions that he sees true showings or false? What choices does he make?&lt;/p&gt;
+&lt;p&gt;When the last candidate Master Monstruwacan of the Lesser Redoubt undergoes the ordeal — he makes a choice. But is the path he chooses painful yet necessary? Or is it a great betrayal?&lt;/p&gt;
+&lt;/div&gt;
+&lt;p class=&quot;feed-readmore&quot;&gt;&lt;a target=&quot;_blank&quot; href=&quot;https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt/291-a-mouse-in-the-walls-of-the-lesser-redoubt-part-1&quot;&gt;Read More ...&lt;/a&gt;&lt;/p&gt;&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;h3 class=&quot;story-teaser&quot;&gt;By &lt;a href=&quot;https://nightland.website/index.php/about/authors-and-artists/109-nigel-atkinson&quot; target=&quot;_blank&quot; rel=&quot;noopener noreferrer&quot;&gt;Nigel Atkinson&lt;/a&gt;&lt;/h3&gt;
+&lt;div class=&quot;story-intro&quot;&gt;
+&lt;p&gt;&lt;img class=&quot;story-thumb&quot; src=&quot;https://nightland.website/images/thumbs/fabian-lesser-redoubt-thumb.jpg&quot; alt=&quot;A traveller beholds a tall pyramid in darkness.&quot; /&gt;&lt;/p&gt;
+&lt;p&gt;In the Lesser Redoubt, when the Master Monstruwacan dies, his designated successor undergoes an ordeal before he can succeed to his office. But who does he converse with? Are the visions that he sees true showings or false? What choices does he make?&lt;/p&gt;
+&lt;p&gt;When the last candidate Master Monstruwacan of the Lesser Redoubt undergoes the ordeal — he makes a choice. But is the path he chooses painful yet necessary? Or is it a great betrayal?&lt;/p&gt;
+&lt;/div&gt;
+&lt;p class=&quot;feed-readmore&quot;&gt;&lt;a target=&quot;_blank&quot; href=&quot;https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt/291-a-mouse-in-the-walls-of-the-lesser-redoubt-part-1&quot;&gt;Read More ...&lt;/a&gt;&lt;/p&gt;&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="A Mouse in the Walls of the Lesser Redoubt" />
+		<category term="The Last Redoubt" />
+		<category term="Night Land Stories" />
+		<category term="Stories" />
+		<category term="ROOT" />
+	</entry>
+	<entry>
+		<title>The Novella is Posted Entire</title>
+		<link rel="alternate" type="text/html" href="https://nightland.website/index.php/journal/29-this-site/301-the-novella-is-posted-entire"/>
+		<published>2022-11-27T05:25:15+00:00</published>
+		<updated>2022-11-27T05:25:15+00:00</updated>
+		<id>https://nightland.website/index.php/journal/29-this-site/301-the-novella-is-posted-entire</id>
+		<author>
+			<name>Kate Coady</name>
+			<email>coady.k@protonmail.com</email>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;em&gt;&lt;a href=&quot;https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt&quot;&gt;A Mouse in the Walls of the Lesser Redoubt&lt;/a&gt;&lt;/em&gt; is complete&lt;/p&gt;
+&lt;p&gt;I have a few more stories and articles to bring back, and an article series to finish.&lt;/p&gt;&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;em&gt;&lt;a href=&quot;https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt&quot;&gt;A Mouse in the Walls of the Lesser Redoubt&lt;/a&gt;&lt;/em&gt; is complete&lt;/p&gt;
+&lt;p&gt;I have a few more stories and articles to bring back, and an article series to finish.&lt;/p&gt;&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="This Site" />
+		<category term="Journal" />
+		<category term="ROOT" />
+	</entry>
+	<entry>
+		<title>I Zigged When I Should Have Zagged</title>
+		<link rel="alternate" type="text/html" href="https://nightland.website/index.php/journal/29-this-site/303-i-zigged-when-i-should-have-zagged"/>
+		<published>2023-09-23T18:54:37+00:00</published>
+		<updated>2023-09-23T18:54:37+00:00</updated>
+		<id>https://nightland.website/index.php/journal/29-this-site/303-i-zigged-when-i-should-have-zagged</id>
+		<author>
+			<name>Kate Coady</name>
+			<email>coady.k@protonmail.com</email>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;Hail, good readers.&lt;/p&gt;
+&lt;p&gt;It's going to look like nought is happening for a time. But it won't — the Almighty willing — be so. I'm at work.&lt;/p&gt;
+&lt;p&gt;About a decade ago I took over the Night Land website. It was crufty old table-layout HTML with some CSS, and it wasn't mobile-friendly. ('Responsive design' is the insufficiently specific buzzphrase.) Its founder, the late and sorely missed Andy Robertson, never had the time to rework it.&lt;/p&gt;
+&lt;p&gt;I decided that the right way to adapt the site for mobile browsing was to convert it to a content management system — mostly because the prospect of converting every page by hand looked daunting. It would, I thought, take too long.&lt;/p&gt;
+&lt;p&gt;I picked Joomla for the CMS — which was the right choice, given its flexibility, the varying layouts of the site's pages. I've had no need to employ &quot;WordPress delenda est!&quot; as a habitual sign-off. Minor updates have worked flawlessly.&lt;/p&gt;
+&lt;p&gt;The current Joomla version has reached end of life. It's time for a major upgrade. I was making one in the test site. That worked, too, in the sense that all the site contents came over just fine. But a couple of the add-ons I use didn't get upgrades. I could live without one, and shift and rejigger the other — which rejiggering would be a blasted time-consuming nuisance, because this isn't my specialty. I could do it, though.&lt;/p&gt;
+&lt;p&gt;Will I be able to do it ten years from now? What about the next Night Land webmaster?&lt;/p&gt;
+&lt;p&gt;That is to say: while Joomla was the right content management system if we were going to have a content management system ... we don't have the right use case for a content management system.&lt;/p&gt;
+&lt;p&gt;The Night Land website isn't a business, where someone (employee or indepent contractor) is paid to attend the site. It's always been tended by literary enthusiasts who might be techies in their day job, but who don't necessarily have the free time to hurry up and fix it if some part of the tech stack breaks. (I might not have been able to fix a serious problem in a timely manner through the late 2010s.)&lt;/p&gt;
+&lt;p&gt;A CMS gives productivity gains if someone's diligently maintaining it. It runs on top of a whole bunch of other packages, which all need to work, which all need to be the right versions. Backing the site up means the whole teeter-totter must be in working order one way. Restoring means it must work again the other way. Do both operations still work if something got updated? Am I sure? If I don't test every so often, I'm risking discovering, after some disaster, that I don't have a backup I can restore from.&lt;/p&gt;
+&lt;p&gt;Contrast that to HTML and simple CSS. The HTML websites I wrote in the 1990s are still completely legible and useable on a desktop, laptop, or tablet. On a phone, you have to scroll and zoom, and one of them is uncommonly annoying ... but you can still read it.&lt;/p&gt;
+&lt;p&gt;30 years of rapid tech change, and I could put those sites up tomorrow, and they'd pretty much work.&lt;/p&gt;
+&lt;p&gt;Also: you can always pull a complete copy of an HTML &amp;amp; simple CSS website down from the front end. The browser sees everything. Nothing depends on a database you can't see, and can't reach if you can't log in.&lt;/p&gt;
+&lt;p&gt;A website made from HTML &amp;amp; simple CSS is not fragile. It's robust. It might show some degradation of function if somebody decides to render it in a way nobody ever rendered websites before. But it can survive prolonged periods of benign neglect.&lt;/p&gt;
+&lt;p&gt;So, with recent hindsight, it's obvious what, a decade ago, I should have done. I'm doing it now.&lt;/p&gt;
+&lt;p&gt;Little or nothing will change visibly for a while, while I write the skeleton of the new HTML/CSS site, and a sample of every kind of content page we're going to need. Then at some point you'll see the major structural pages switch all at once, followed by relatively rapid reloading of content pages.&lt;/p&gt;
+&lt;p&gt;The design is going to retain some features of the current pages, for mobile browsing. But in some ways it will call to mind Andy's original site more than the Joomla template does.&lt;/p&gt;
+&lt;p&gt;I have no set time to finish. It will probably happen over the next few months.&lt;/p&gt;&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;Hail, good readers.&lt;/p&gt;
+&lt;p&gt;It's going to look like nought is happening for a time. But it won't — the Almighty willing — be so. I'm at work.&lt;/p&gt;
+&lt;p&gt;About a decade ago I took over the Night Land website. It was crufty old table-layout HTML with some CSS, and it wasn't mobile-friendly. ('Responsive design' is the insufficiently specific buzzphrase.) Its founder, the late and sorely missed Andy Robertson, never had the time to rework it.&lt;/p&gt;
+&lt;p&gt;I decided that the right way to adapt the site for mobile browsing was to convert it to a content management system — mostly because the prospect of converting every page by hand looked daunting. It would, I thought, take too long.&lt;/p&gt;
+&lt;p&gt;I picked Joomla for the CMS — which was the right choice, given its flexibility, the varying layouts of the site's pages. I've had no need to employ &quot;WordPress delenda est!&quot; as a habitual sign-off. Minor updates have worked flawlessly.&lt;/p&gt;
+&lt;p&gt;The current Joomla version has reached end of life. It's time for a major upgrade. I was making one in the test site. That worked, too, in the sense that all the site contents came over just fine. But a couple of the add-ons I use didn't get upgrades. I could live without one, and shift and rejigger the other — which rejiggering would be a blasted time-consuming nuisance, because this isn't my specialty. I could do it, though.&lt;/p&gt;
+&lt;p&gt;Will I be able to do it ten years from now? What about the next Night Land webmaster?&lt;/p&gt;
+&lt;p&gt;That is to say: while Joomla was the right content management system if we were going to have a content management system ... we don't have the right use case for a content management system.&lt;/p&gt;
+&lt;p&gt;The Night Land website isn't a business, where someone (employee or indepent contractor) is paid to attend the site. It's always been tended by literary enthusiasts who might be techies in their day job, but who don't necessarily have the free time to hurry up and fix it if some part of the tech stack breaks. (I might not have been able to fix a serious problem in a timely manner through the late 2010s.)&lt;/p&gt;
+&lt;p&gt;A CMS gives productivity gains if someone's diligently maintaining it. It runs on top of a whole bunch of other packages, which all need to work, which all need to be the right versions. Backing the site up means the whole teeter-totter must be in working order one way. Restoring means it must work again the other way. Do both operations still work if something got updated? Am I sure? If I don't test every so often, I'm risking discovering, after some disaster, that I don't have a backup I can restore from.&lt;/p&gt;
+&lt;p&gt;Contrast that to HTML and simple CSS. The HTML websites I wrote in the 1990s are still completely legible and useable on a desktop, laptop, or tablet. On a phone, you have to scroll and zoom, and one of them is uncommonly annoying ... but you can still read it.&lt;/p&gt;
+&lt;p&gt;30 years of rapid tech change, and I could put those sites up tomorrow, and they'd pretty much work.&lt;/p&gt;
+&lt;p&gt;Also: you can always pull a complete copy of an HTML &amp;amp; simple CSS website down from the front end. The browser sees everything. Nothing depends on a database you can't see, and can't reach if you can't log in.&lt;/p&gt;
+&lt;p&gt;A website made from HTML &amp;amp; simple CSS is not fragile. It's robust. It might show some degradation of function if somebody decides to render it in a way nobody ever rendered websites before. But it can survive prolonged periods of benign neglect.&lt;/p&gt;
+&lt;p&gt;So, with recent hindsight, it's obvious what, a decade ago, I should have done. I'm doing it now.&lt;/p&gt;
+&lt;p&gt;Little or nothing will change visibly for a while, while I write the skeleton of the new HTML/CSS site, and a sample of every kind of content page we're going to need. Then at some point you'll see the major structural pages switch all at once, followed by relatively rapid reloading of content pages.&lt;/p&gt;
+&lt;p&gt;The design is going to retain some features of the current pages, for mobile browsing. But in some ways it will call to mind Andy's original site more than the Joomla template does.&lt;/p&gt;
+&lt;p&gt;I have no set time to finish. It will probably happen over the next few months.&lt;/p&gt;&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="This Site" />
+		<category term="Journal" />
+		<category term="ROOT" />
+	</entry>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>The Night Land - Home</title>
+		<description><![CDATA[The Weird Fiction of William Hope Hodgson]]></description>
+		<link>https://nightland.website/index.php</link>
+		<lastBuildDate>Mon, 21 Apr 2025 00:27:02 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="https://nightland.website/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+		<item>
+			<title>A Mouse in the Walls of the Lesser Redoubt (Part 1)</title>
+			<link>https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt/291-a-mouse-in-the-walls-of-the-lesser-redoubt-part-1</link>
+			<guid isPermaLink="true">https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt/291-a-mouse-in-the-walls-of-the-lesser-redoubt-part-1</guid>
+			<description><![CDATA[<div class="feed-description"><h3 class="story-teaser">By <a href="https://nightland.website/index.php/about/authors-and-artists/109-nigel-atkinson" target="_blank" rel="noopener noreferrer">Nigel Atkinson</a></h3>
+<div class="story-intro">
+<p><img class="story-thumb" src="https://nightland.website/images/thumbs/fabian-lesser-redoubt-thumb.jpg" alt="A traveller beholds a tall pyramid in darkness." /></p>
+<p>In the Lesser Redoubt, when the Master Monstruwacan dies, his designated successor undergoes an ordeal before he can succeed to his office. But who does he converse with? Are the visions that he sees true showings or false? What choices does he make?</p>
+<p>When the last candidate Master Monstruwacan of the Lesser Redoubt undergoes the ordeal — he makes a choice. But is the path he chooses painful yet necessary? Or is it a great betrayal?</p>
+</div>
+<p class="feed-readmore"><a target="_blank" href="https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt/291-a-mouse-in-the-walls-of-the-lesser-redoubt-part-1">Read More ...</a></p></div>]]></description>
+			<author>coady.k@protonmail.com (Nigel Atkinson)</author>
+			<category>Featured</category>
+			<category>A Mouse in the Walls of the Lesser Redoubt</category>
+			<category>The Last Redoubt</category>
+			<category>Night Land Stories</category>
+			<category>Stories</category>
+			<category>ROOT</category>
+			<pubDate>Fri, 08 Jul 2022 21:44:14 +0000</pubDate>
+		</item>
+		<item>
+			<title>The Novella is Posted Entire</title>
+			<link>https://nightland.website/index.php/journal/29-this-site/301-the-novella-is-posted-entire</link>
+			<guid isPermaLink="true">https://nightland.website/index.php/journal/29-this-site/301-the-novella-is-posted-entire</guid>
+			<description><![CDATA[<div class="feed-description"><p><em><a href="https://nightland.website/index.php/stories/night-land-stories/the-last-redoubt/86-a-mouse-in-the-walls-of-the-lesser-redoubt">A Mouse in the Walls of the Lesser Redoubt</a></em> is complete</p>
+<p>I have a few more stories and articles to bring back, and an article series to finish.</p></div>]]></description>
+			<author>coady.k@protonmail.com (Kate Coady)</author>
+			<category>Featured</category>
+			<category>This Site</category>
+			<category>Journal</category>
+			<category>ROOT</category>
+			<pubDate>Sun, 27 Nov 2022 05:25:15 +0000</pubDate>
+		</item>
+		<item>
+			<title>I Zigged When I Should Have Zagged</title>
+			<link>https://nightland.website/index.php/journal/29-this-site/303-i-zigged-when-i-should-have-zagged</link>
+			<guid isPermaLink="true">https://nightland.website/index.php/journal/29-this-site/303-i-zigged-when-i-should-have-zagged</guid>
+			<description><![CDATA[<div class="feed-description"><p>Hail, good readers.</p>
+<p>It's going to look like nought is happening for a time. But it won't — the Almighty willing — be so. I'm at work.</p>
+<p>About a decade ago I took over the Night Land website. It was crufty old table-layout HTML with some CSS, and it wasn't mobile-friendly. ('Responsive design' is the insufficiently specific buzzphrase.) Its founder, the late and sorely missed Andy Robertson, never had the time to rework it.</p>
+<p>I decided that the right way to adapt the site for mobile browsing was to convert it to a content management system — mostly because the prospect of converting every page by hand looked daunting. It would, I thought, take too long.</p>
+<p>I picked Joomla for the CMS — which was the right choice, given its flexibility, the varying layouts of the site's pages. I've had no need to employ "WordPress delenda est!" as a habitual sign-off. Minor updates have worked flawlessly.</p>
+<p>The current Joomla version has reached end of life. It's time for a major upgrade. I was making one in the test site. That worked, too, in the sense that all the site contents came over just fine. But a couple of the add-ons I use didn't get upgrades. I could live without one, and shift and rejigger the other — which rejiggering would be a blasted time-consuming nuisance, because this isn't my specialty. I could do it, though.</p>
+<p>Will I be able to do it ten years from now? What about the next Night Land webmaster?</p>
+<p>That is to say: while Joomla was the right content management system if we were going to have a content management system ... we don't have the right use case for a content management system.</p>
+<p>The Night Land website isn't a business, where someone (employee or indepent contractor) is paid to attend the site. It's always been tended by literary enthusiasts who might be techies in their day job, but who don't necessarily have the free time to hurry up and fix it if some part of the tech stack breaks. (I might not have been able to fix a serious problem in a timely manner through the late 2010s.)</p>
+<p>A CMS gives productivity gains if someone's diligently maintaining it. It runs on top of a whole bunch of other packages, which all need to work, which all need to be the right versions. Backing the site up means the whole teeter-totter must be in working order one way. Restoring means it must work again the other way. Do both operations still work if something got updated? Am I sure? If I don't test every so often, I'm risking discovering, after some disaster, that I don't have a backup I can restore from.</p>
+<p>Contrast that to HTML and simple CSS. The HTML websites I wrote in the 1990s are still completely legible and useable on a desktop, laptop, or tablet. On a phone, you have to scroll and zoom, and one of them is uncommonly annoying ... but you can still read it.</p>
+<p>30 years of rapid tech change, and I could put those sites up tomorrow, and they'd pretty much work.</p>
+<p>Also: you can always pull a complete copy of an HTML &amp; simple CSS website down from the front end. The browser sees everything. Nothing depends on a database you can't see, and can't reach if you can't log in.</p>
+<p>A website made from HTML &amp; simple CSS is not fragile. It's robust. It might show some degradation of function if somebody decides to render it in a way nobody ever rendered websites before. But it can survive prolonged periods of benign neglect.</p>
+<p>So, with recent hindsight, it's obvious what, a decade ago, I should have done. I'm doing it now.</p>
+<p>Little or nothing will change visibly for a while, while I write the skeleton of the new HTML/CSS site, and a sample of every kind of content page we're going to need. Then at some point you'll see the major structural pages switch all at once, followed by relatively rapid reloading of content pages.</p>
+<p>The design is going to retain some features of the current pages, for mobile browsing. But in some ways it will call to mind Andy's original site more than the Joomla template does.</p>
+<p>I have no set time to finish. It will probably happen over the next few months.</p></div>]]></description>
+			<author>coady.k@protonmail.com (Kate Coady)</author>
+			<category>Featured</category>
+			<category>This Site</category>
+			<category>Journal</category>
+			<category>ROOT</category>
+			<pubDate>Sat, 23 Sep 2023 18:54:37 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
